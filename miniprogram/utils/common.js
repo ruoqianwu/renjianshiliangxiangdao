a1b716ce235790ef4a781f1473c914f819b95ca8
ruoqianwu <raymondng0502@gmail.com>
2021-01-07 19:58:44 +1100

    fixed

--- miniprogram/utils/common.js
+++ miniprogram/utils/common.js
@@ -1,19 +1,7 @@
 
 // get access key method
 async function refreshAccessToken() {
-  //const cloud = require('wx-server-sdk');
-  // if (!wx.cloud) {
-  //   console.error('请使用 2.2.3 或以上的基础库以使用云能力')
-  // } else {
-  //   wx.cloud.init({
-  //     // env 参数说明：
-  //     //   env 参数决定接下来小程序发起的云开发调用（wx.cloud.xxx）会默认请求到哪个云环境的资源
-  //     //   此处请填入环境 ID, 环境 ID 可打开云控制台查看
-  //     //   如不填则使用默认环境（第一个创建的环境）
-  //     env: 'home-2gkxjfu87d968e84',
-  //     traceUser: true,
-  //   });
-  // }
+
   const db = wx.cloud.database();
   const previous = async() => {
     return new Promise((resolve, reject) => {
@@ -48,7 +36,6 @@ async function refreshAccessToken() {
     }
   
     const res = await rp();
-   // console.log(res)
     db.collection('access_token').doc('a8831daa5fda92920006174f38470fcc').update({
       data: {
         accessToken: res.data.access_token,
@@ -59,66 +46,6 @@ async function refreshAccessToken() {
     return res.data.access_token;
   
 }
-
-// async function fetchArticles(access) {
-//   wx.request({
-//     url: 'https://api.weixin.qq.com/cgi-bin/material/batchget_material?access_token=' + access,
-//     data: {
-//       type: 'news',
-//       offset: 0,
-//       count: 1
-//     },
-//     method: 'POST',
-//     success: res => {
-//       let totalCount = res.data.total_count;
-//       /**
-//        * 结构：{
-//        *  文章id，
-//        *  作者，
-//        *  标题，
-//        *  摘要，
-//        *  封面图URL，
-//        *  文章url
-//        * }
-//        */
-//       let art = {
-//         media_id: res.data.item[0].media_id,
-//         author: res.data.item[0].content.news_item[0].author, 
-//         title: res.data.item[0].content.news_item[0].title, 
-//         digest: res.data.item[0].content.news_item[0].digest,
-//         thumb_url: res.data.item[0].content.news_item[0].thumb_url,
-//         url: res.data.item[0].content.news_item[0].url,
-//       };
-//       console.log(art)
-//       for (let i = 1; i < totalCount; i++){
-//         let article = {
-//           type: 'news',
-//           offset: i,
-//           count: 1
-//         };
-//         wx.request({
-//           url: 'https://api.weixin.qq.com/cgi-bin/material/batchget_material?access_token=' + access,
-//           data: article,
-//           method: 'POST',
-//           success: res => {
-//             let art = {
-//               media_id: res.data.item[0].media_id,
-//               author: res.data.item[0].content.news_item[0].author, 
-//               title: res.data.item[0].content.news_item[0].title, 
-//               digest: res.data.item[0].content.news_item[0].digest,
-//               thumb_url: res.data.item[0].content.news_item[0].thumb_url,
-//               url: res.data.item[0].content.news_item[0].url,
-//             };
-//             db.collection('articles').add({
-//               data: art,
-//               success: res => console.log(res)
-//             }) 
-//           }
-//         });
-//       }
-//     }
-//   });
-// }
 
 function addToDb(object) {
   const db = wx.cloud.database();
@@ -145,6 +72,5 @@ function shuffle(array) {
 }
 
 module.exports.refreshAccessToken = refreshAccessToken;
-//module.exports.fetchArticles = fetchArticles;
 module.exports.addToDb = addToDb;
 module.exports.shuffle = shuffle;
